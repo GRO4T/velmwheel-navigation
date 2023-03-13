cff43e18bc81318a708e7436631f57bd0d3a7a2d
Damian Kolaska <damian.kolaska99@gmail.com>
2023-03-13 08:13:18 +0100

    velmwheel_gazebo: Added contact plugin which detects collisions with robot's base

---
+++ src/velmwheel/sim/velmwheel_gazebo/velmwheel_gazebo/src/contact_gazebo.cpp
@@ -0,0 +1,68 @@
+#include "velmwheel/gazebo/contact_gazebo.hpp"
+
+using namespace gazebo;
+GZ_REGISTER_SENSOR_PLUGIN(ContactPlugin)
+
+/////////////////////////////////////////////////
+ContactPlugin::ContactPlugin() : SensorPlugin()
+{
+}
+
+/////////////////////////////////////////////////
+ContactPlugin::~ContactPlugin()
+{
+}
+
+/////////////////////////////////////////////////
+void ContactPlugin::Load(sensors::SensorPtr _sensor, sdf::ElementPtr _sdf)
+{
+  node = gazebo_ros::Node::Get(_sdf);
+
+  RCLCPP_INFO_STREAM(node->get_logger(), "Loading contact plugin");
+
+  // Get the parent sensor.
+  this->parentSensor =
+    std::dynamic_pointer_cast<sensors::ContactSensor>(_sensor);
+
+  // Make sure the parent sensor is valid.
+  if (!this->parentSensor)
+  {
+    RCLCPP_ERROR_STREAM(node->get_logger(), "ContactPlugin requires a ContactSensor.");
+    return;
+  }
+
+  // Connect to the sensor update event.
+  this->updateConnection = this->parentSensor->ConnectUpdated(
+      std::bind(&ContactPlugin::OnUpdate, this));
+
+  // Make sure the parent sensor is active.
+  this->parentSensor->SetActive(true);
+
+  RCLCPP_INFO_STREAM(node->get_logger(), "Contact plugin loaded");
+}
+
+/////////////////////////////////////////////////
+void ContactPlugin::OnUpdate()
+{
+  // Get all the contacts.
+  msgs::Contacts contacts;
+  contacts = this->parentSensor->Contacts();
+  for (unsigned int i = 0; i < contacts.contact_size(); ++i)
+  {
+    std::cout << "Collision between[" << contacts.contact(i).collision1()
+              << "] and [" << contacts.contact(i).collision2() << "]\n";
+
+    for (unsigned int j = 0; j < contacts.contact(i).position_size(); ++j)
+    {
+      std::cout << j << "  Position:"
+                << contacts.contact(i).position(j).x() << " "
+                << contacts.contact(i).position(j).y() << " "
+                << contacts.contact(i).position(j).z() << "\n";
+      std::cout << "   Normal:"
+                << contacts.contact(i).normal(j).x() << " "
+                << contacts.contact(i).normal(j).y() << " "
+                << contacts.contact(i).normal(j).z() << "\n";
+      std::cout << "   Depth:" << contacts.contact(i).depth(j) << "\n";
+    }
+  }
+}
