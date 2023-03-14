c3f38ee4e9f1fd9faad33e06fcf1afbbd1afc2e0
Damian Kolaska <damian.kolaska99@gmail.com>
2023-03-14 20:44:14 +0100

    contact_plugin: Publishing custom message containing contact state + refactoring

--- src/velmwheel/sim/velmwheel_gazebo/velmwheel_gazebo/include/velmwheel/gazebo/contact_gazebo.hpp
+++ src/velmwheel/sim/velmwheel_gazebo/velmwheel_gazebo/include/velmwheel/gazebo/contact_gazebo.hpp
@@ -1,39 +1,94 @@
-#ifndef _GAZEBO_CONTACT_PLUGIN_HH_
-#define _GAZEBO_CONTACT_PLUGIN_HH_
+/* ============================================================================================================================ *//**
+ * @file       contact_gazebo.hpp
+ * @author     Damian Kolaska (damian.kolaska99@gmail.com)
+ * @maintainer Damian Kolaska (damian.kolaska99@gmail.com)
+ * @date       Sunday, 12th March 2023 11:02:23 am
+ * @modified   Tuesday, 14th March 2023 8:28:23 pm
+ * @project    velmwheel
+ * @brief      
+ * 
+ * 
+ * @copyright Damian Kolaska © 2023
+ */// ============================================================================================================================= */
 
+#ifndef __VELMWHEEL_GAZEBO_CONTACT_PLUGIN_H__
+#define __VELMWHEEL_GAZEBO_CONTACT_PLUGIN_H__
+
+/* =========================================================== Includes =========================================================== */
+
+// System includes
 #include <string>
-
+// Gazebo includes
 #include <gazebo/gazebo.hh>
 #include <gazebo/sensors/sensors.hh>
 #include <gazebo_ros/node.hpp>
+// ROS includes
+#include <rclcpp/rclcpp.hpp>
+// Private includes
+#include "velmwheel_gazebo_msgs/msg/contact_state.hpp"
+
+/* ========================================================== Namespaces ========================================================== */
 
 namespace gazebo
 {
-  /// \brief An example plugin for a contact sensor.
-  class ContactPlugin : public SensorPlugin
-  {
-    /// \brief Constructor.
-    public: ContactPlugin();
 
-    /// \brief Destructor.
-    public: virtual ~ContactPlugin();
+/* ============================================================= Nodes ============================================================ */
 
-    /// \brief Load the sensor plugin.
-    /// \param[in] _sensor Pointer to the sensor that loaded this plugin.
-    /// \param[in] _sdf SDF element that describes the plugin.
-    public: virtual void Load(sensors::SensorPtr _sensor, sdf::ElementPtr _sdf);
+/**
+ * @brief TODO(damiankolaska): add docstring
+ */
+class ContactPlugin : public SensorPlugin {
 
-    /// \brief Callback that receives the contact sensor's update signal.
-    private: virtual void OnUpdate();
+public: /* ------------------------------------------------ Topic's parameters ---------------------------------------------------- */
+    static constexpr std::size_t TOPIC_QUEUE_SIZE = 1000;
 
-    /// \brief Pointer to the contact sensor
-    private: sensors::ContactSensorPtr parentSensor;
+    static constexpr auto OUT_TOPIC_NAME = "contacts";
 
-    /// \brief Connection that maintains a link between the contact sensor's
-    /// updated signal and the OnUpdate callback.
-    private: event::ConnectionPtr updateConnection;
+public: /* ----------------------------------------------- Public ctors & dtors --------------------------------------------------- */
 
-    private: gazebo_ros::Node::SharedPtr node;
+    /**
+     * @brief Construct a new Plugin object
+     */
+    ContactPlugin() = default;
+
+private: /* -------------------------------------------- Gazebo-specific methods -------------------------------------------------- */
+
+	/**
+	 * @brief On-load Gazebo initialization routine of the plugin
+     * @param parent 
+     *    pointer to the Gazebo Sensor model (injected by the Gazebo)
+     * @param sdf
+     *    pointer to the SDF <plugin> element of the sensor's description (injected by the Gazebo)
+	 */
+    void Load(sensors::SensorPtr sensor, sdf::ElementPtr sdf);
+
+	/**
+	 * @brief On simulation update transfers IMU measurements to the ROS topic
+	 */
+    void OnUpdate();
+
+private: /* ------------------------------------------------- ROS interfaces ------------------------------------------------------ */
+
+    /// ROS node
+    gazebo_ros::Node::SharedPtr node;
+
+private: /* -------------------------------------------------- Node's state ------------------------------------------------------- */
+
+	/// Reference to the Gazebo contact sensor
+    sensors::ContactSensorPtr sensor;
+    /// TODO(damiankolaska) add docstring
+    rclcpp::Publisher<velmwheel_gazebo_msgs::msg::ContactState>::SharedPtr pub;
+
+    /**
+     * @brief Connection that maintains a link between the contact sensor's
+     * updated signal and the OnUpdate callback.
+    */
+    event::ConnectionPtr sensor_update_connection;
+
   };
-}
+
+/* ================================================================================================================================ */
+
+} // End namespace gazebo
+
 #endif
--- src/velmwheel/sim/velmwheel_gazebo/velmwheel_gazebo/src/contact_gazebo.cpp
+++ src/velmwheel/sim/velmwheel_gazebo/velmwheel_gazebo/src/contact_gazebo.cpp
@@ -1,68 +1,101 @@
-#include "velmwheel/gazebo/contact_gazebo.hpp"
+/* ============================================================================================================================ *//**
+ * @file       contact_gazebo.cpp
+ * @author     Damian Kolaska (damian.kolaska99@gmail.com)
+ * @maintainer Damian Kolaska (damian.kolaska99@gmail.com)
+ * @date       Sunday, 12th March 2023 3:00:51 pm
+ * @modified   Tuesday, 14th March 2023 8:40:58 pm
+ * @project    velmwheel
+ * @brief      TODO(damiankolaska) - add brief description
+ * 
+ * 
+ * @copyright Damian Kolaska © 2023
+ */// ============================================================================================================================= */
 
-using namespace gazebo;
+/* =========================================================== Includes =========================================================== */
+
+#include "velmwheel/gazebo/contact_gazebo.hpp"
+#include "node_common/communication.hpp"
+
+/* ========================================================== Namespaces ========================================================== */
+
+namespace gazebo {
+
+void ContactPlugin::Load(sensors::SensorPtr sensor, sdf::ElementPtr sdf) {
+    /* ------------------------- Initialize ROS interface ------------------------ */
+    
+    // Create ROS node (Create the node via the Gazebo interface to automatically handle namespace, remaps, etc...)
+    node = gazebo_ros::Node::Get(sdf);
+
+    *node_common::communication::make_publisher_builder(pub)
+        .node(*node)
+        .name(OUT_TOPIC_NAME)
+        .qos(TOPIC_QUEUE_SIZE);
+
+    this->sensor =
+        std::dynamic_pointer_cast<sensors::ContactSensor>(sensor);
+
+    if (!this->sensor)
+    {
+        RCLCPP_ERROR_STREAM(node->get_logger(), "ContactPlugin requires a sensor.");
+        return;
+    }
+
+    // Connect to the sensor update event.
+    this->sensor_update_connection = this->sensor->ConnectUpdated(
+        std::bind(&ContactPlugin::OnUpdate, this));
+
+    // Make sure the parent sensor is active.
+    this->sensor->SetActive(true);
+
+    RCLCPP_INFO_STREAM(node->get_logger(), "Running 'contact_gazebo' plugin...");
+}
+
+void ContactPlugin::OnUpdate() {
+  msgs::Contacts contacts = this->sensor->Contacts();
+  for (unsigned int i = 0; i < contacts.contact_size(); ++i) {
+    const auto& contact = contacts.contact(i);
+
+    RCLCPP_DEBUG_STREAM(
+        node->get_logger(),
+        "Collision between[" << contact.collision1()
+            << "] and [" << contact.collision2() << "]\n"
+    );
+
+    for (unsigned int j = 0; j < contact.position_size(); ++j)
+    {
+        RCLCPP_DEBUG_STREAM(
+            node->get_logger(),
+            j << "  Position:"
+                << contact.position(j).x() << " "
+                << contact.position(j).y() << " "
+                << contact.position(j).z() << "\n"
+        );
+        RCLCPP_DEBUG_STREAM(
+            node->get_logger(),
+            "   Normal:"
+                << contact.normal(j).x() << " "
+                << contact.normal(j).y() << " "
+                << contact.normal(j).z() << "\n";
+        );
+        RCLCPP_DEBUG_STREAM(
+            node->get_logger(),
+            "   Depth:" << contact.depth(j) << "\n"
+        );
+    }
+
+    velmwheel_gazebo_msgs::msg::ContactState msg;
+    msg.info = "Velmwheel robot collision";
+    msg.collision1_name = contact.collision1();
+    msg.collision2_name = contact.collision2();
+    pub->publish(msg);
+  }
+}
+
+/* ================================================================================================================================ */
+
+// Register class as Gazebo plugin
 GZ_REGISTER_SENSOR_PLUGIN(ContactPlugin)
 
-/////////////////////////////////////////////////
-ContactPlugin::ContactPlugin() : SensorPlugin()
-{
-}
+/* ================================================================================================================================ */
 
-/////////////////////////////////////////////////
-ContactPlugin::~ContactPlugin()
-{
-}
-
-/////////////////////////////////////////////////
-void ContactPlugin::Load(sensors::SensorPtr _sensor, sdf::ElementPtr _sdf)
-{
-  node = gazebo_ros::Node::Get(_sdf);
-
-  RCLCPP_INFO_STREAM(node->get_logger(), "Loading contact plugin");
-
-  // Get the parent sensor.
-  this->parentSensor =
-    std::dynamic_pointer_cast<sensors::ContactSensor>(_sensor);
-
-  // Make sure the parent sensor is valid.
-  if (!this->parentSensor)
-  {
-    RCLCPP_ERROR_STREAM(node->get_logger(), "ContactPlugin requires a ContactSensor.");
-    return;
-  }
-
-  // Connect to the sensor update event.
-  this->updateConnection = this->parentSensor->ConnectUpdated(
-      std::bind(&ContactPlugin::OnUpdate, this));
-
-  // Make sure the parent sensor is active.
-  this->parentSensor->SetActive(true);
-
-  RCLCPP_INFO_STREAM(node->get_logger(), "Contact plugin loaded");
-}
-
-/////////////////////////////////////////////////
-void ContactPlugin::OnUpdate()
-{
-  // Get all the contacts.
-  msgs::Contacts contacts;
-  contacts = this->parentSensor->Contacts();
-  for (unsigned int i = 0; i < contacts.contact_size(); ++i)
-  {
-    std::cout << "Collision between[" << contacts.contact(i).collision1()
-              << "] and [" << contacts.contact(i).collision2() << "]\n";
-
-    for (unsigned int j = 0; j < contacts.contact(i).position_size(); ++j)
-    {
-      std::cout << j << "  Position:"
-                << contacts.contact(i).position(j).x() << " "
-                << contacts.contact(i).position(j).y() << " "
-                << contacts.contact(i).position(j).z() << "\n";
-      std::cout << "   Normal:"
-                << contacts.contact(i).normal(j).x() << " "
-                << contacts.contact(i).normal(j).y() << " "
-                << contacts.contact(i).normal(j).z() << "\n";
-      std::cout << "   Depth:" << contacts.contact(i).depth(j) << "\n";
-    }
-  }
-}
+} // End namespace gazebo
